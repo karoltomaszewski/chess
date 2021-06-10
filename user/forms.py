eb0c2a45beaed0ba2634669d6b3414e5117ae7bf
uwuziom <jbak663@gmail.com>
2021-06-10 14:12:48 +0200

    Pierwszy wyglad forma

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
+from typing import ClassVar
 from django import forms
 
 class MainForm(forms.Form):
-    username = forms.CharField(required=True)
+    username = forms.CharField(required=True, label='Nickname')
